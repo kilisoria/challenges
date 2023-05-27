64f537b492d4e7efb901b825753e230e352f1e68
Cristians Soria <cristiansoria@gmail.com>
2023-05-26 21:28:59 -0300

    Add more code and clean code

--- javascript/src/play02.js
+++
@@ -1,6 +0,0 @@
-
-sequenceSum = (b, e, s) => b > e ? 0 : b + sequenceSum(b + s, e, s);
-
-console.log(sequenceSum(1, 5, 1));
-console.log(sequenceSum(2, 6, 2));
-console.log(sequenceSum(1, 5, 3));
--- javascript/src/play04.js
+++
@@ -1,4 +0,0 @@
-disemvowel = str => str.replace(/[aeiou]/gi, '');
-
-// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-
