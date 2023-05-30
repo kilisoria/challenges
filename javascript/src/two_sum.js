5fb7a65cf96990299280b627aaf58470e16b7dca
Cristians Soria <cristiansoria@gmail.com>
2023-05-30 17:55:15 -0300

    Add test to check the default test runner

--- javascript/src/two_sum.js
+++ javascript/src/two_sum.js
@@ -7,6 +7,9 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 */
 
+const test = require('node:test');
+const assert = require('node:assert');
+
 const twoSum = (nums, target) => {
     let numb1;
     let numb2;
@@ -20,7 +23,7 @@ const twoSum = (nums, target) => {
         
         for (let j = 1 + i; j < nums.length; j++) {
             numb2 = nums[j];
-            
+
             if (typeof numb2 !== 'number' || !Number.isInteger(numb2)) {
                 continue;
             }
@@ -33,6 +36,8 @@ const twoSum = (nums, target) => {
     }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([3,2,4], 6));
-console.log(twoSum([3, 3], 6));
+test('twoSum', () => {
+    assert.deepEqual(twoSum([2, 7, 11, 15], 9), [0, 1]);
+    assert.deepEqual(twoSum([3,2,4], 6), [1, 2]);
+    assert.deepEqual(twoSum([3, 3], 6), [0, 1]);
+});
