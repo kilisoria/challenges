51187b3aa08636f29c8be35c483f0bc9079cd25b
Cristians Soria <cristiansoria@gmail.com>
2023-06-15 12:25:29 -0300

    simple test

--- javascript/src/checking.js
+++ javascript/src/checking.js
@@ -6,3 +6,14 @@ var var1;
 
 // console.log(var3);
 // let var3;
+
+const arrow_fn_test = () => {
+    console.log(arguments);
+};
+
+function fn_test() {
+    console.log(arguments);
+};
+
+arrow_fn_test('aws', 'gcp', 'azure');
+// fn_test('cristian', 'soria', 'engineer', 43);
