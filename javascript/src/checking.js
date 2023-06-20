af86247d0ca9910995cda5294d75d410d0bfd5df
Cristians Soria <cristiansoria@gmail.com>
2023-06-20 18:19:09 -0300

    add new examples

--- javascript/src/checking.js
+++ javascript/src/checking.js
@@ -17,3 +17,28 @@ function fn_test() {
 
 arrow_fn_test('aws', 'gcp', 'azure');
 // fn_test('cristian', 'soria', 'engineer', 43);
+
+
+function foo(num) {
+	console.log( "foo: " + num );
+
+	// keep track of how many times `foo` is called
+	this.count++;
+}
+
+foo.count = 0;
+
+var i;
+
+for (i=0; i<10; i++) {
+	if (i > 5) {
+		foo( i );
+	}
+}
+// foo: 6
+// foo: 7
+// foo: 8
+// foo: 9
+
+// how many times was `foo` called?
+console.log( foo.count ); // 0 -- WTF?
