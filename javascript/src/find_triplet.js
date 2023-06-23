384e8670a1d6172d8000f04d5e97955c16418173
Cristians Soria <cristiansoria@gmail.com>
2023-06-22 21:57:37 -0300

    Update code

--- javascript/src/find_triplet.js
+++ javascript/src/find_triplet.js
@@ -1,9 +1,9 @@
 
-// - Write a function that returns a triplet of numbers from the supplied array, 
+// Write a function that returns a triplet of numbers from the supplied array, 
 // with a sum equal to a specific integer.
 
-const array = [4, 1, 9, 8, 5, 2, 3, 23, 50, 22, 19, 10];
-const int = 35;
+const test = require('node:test');
+const assert = require('node:assert');
 
 // console.log(find_triplet(array,int)) // [ 10, 22, 3 ]
 // console.log(find_triplet(array,int)) // null
@@ -32,8 +32,31 @@ const find_triplet = (array, sumToCheck) => {
         }
     }
  
-    // Not found
+    // Not found.
     return [];
 }
 
-console.log(find_triplet(array, int));
+
+test('Found passing test', (t) => {
+    const array = [4, 1, 9, 8, 5, 2, 3, 23, 50, 22, 19, 10];
+    const sum = 35;
+
+    const numbersMatched = find_triplet(array, sum);
+    assert.deepEqual(numbersMatched, [2, 10, 23]);
+});
+
+test('Alternative found passing test', (t) => {
+    const array = [4, 1, 9, 8, 5, 2, 3, 23, 50, 22, 19, 10];
+    const sum = 69;
+
+    const numbersMatched = find_triplet(array, sum);
+    assert.deepEqual(numbersMatched, [9, 10, 50]);
+});
+
+test('Not found passing test', (t) => {
+    const array = [4, 1, 9, 8, 5, 2, 3, 23, 50, 22, 20];
+    const sum = 999;
+
+    const numbersMatched = find_triplet(array, sum);
+    assert.deepEqual(numbersMatched, []);
+});
